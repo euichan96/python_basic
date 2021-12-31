f58fbb0cb8084bdd3630f9c135a2a415544b22bd
euichan <qkrdmlcks1@naver.com>
2021-12-31 17:39:57 +0900

    pandas day_1

---
+++ test_package_/module_basic/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import test_module as test
+
+radius = test.number_input()
+
+print(test.get_circum(radius))
+print(test.get_circle_area(radius))
+
---
+++ test_package_/test_package/__init__.py
@@ -0,0 +1,4 @@
+# from test_package import * 로 모듈을 읽고자 할 경우
+__all__ = ["module_a", "module_b"]
+print("init 실행 ")
+
